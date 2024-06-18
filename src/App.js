9288a71d55d0bb3b2c41cbfe083e079f17bb5df7
PrasannaAdabala <prasannaadabala385@gmail.com>
2024-06-18 14:35:02 +0530

    componentscreated

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import { Form } from "./components/button.js";
+
+function App(){
+  return(
+    <div>
+    <Form></Form>
+    </div>
+  );
+}
+export default App;
+
---
+++ src/components/button.js
@@ -0,0 +1,25 @@
+
+
+
+ 
+export const Form=()=>{
+  return (
+    <div>
+    <table style={{border:"2px solid"}}>
+        <tr>
+          <th  style={{border:"2px solid"}}>name</th>
+          <th  style={{border:"2px solid"}}>id</th>
+          <th  style={{border:"2px solid"}}>phone number</th>
+          </tr>
+          <tr>
+            <td  style={{border:"2px solid"}}>prasanna</td>
+            <td  style={{border:"2px solid"}}>1</td>
+            <td  style={{border:"2px solid"}}>123456</td>
+          </tr>
+         
+    </table>
+    </div>
+  );
+
+};
+
